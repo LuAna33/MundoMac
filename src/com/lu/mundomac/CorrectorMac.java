4383a05535e67db4f0aa9b06adc548432fff1d70
Luisa Garin <luy22@live.com.ar>
2020-11-04 20:41:40 -0300

    Creacion y codificacion clases Base y Corrector

--- src/com/lu/mundomac/CorrectorMac.java
+++ src/com/lu/mundomac/CorrectorMac.java
@@ -1,4 +1,33 @@
 package com.lu.mundomac;
 
 public class CorrectorMac {
-}
+
+   private int idCorrectorMac;
+   private String colorCorrector;
+   private double precioCorrector;
+   private static int contadorCorrectorMac;
+
+    public CorrectorMac(String colorCorrector, double precioCorrector) {
+        this.colorCorrector = colorCorrector;
+        this.precioCorrector = precioCorrector;
+
+    }
+    public String getColorCorrector() {
+        return colorCorrector;
+    }
+
+    public void setColorCorrector(String colorCorrector) {
+        this.colorCorrector = colorCorrector;
+    }
+
+    public double getPrecioCorrector() {
+        return precioCorrector;
+    }
+
+    public void setPrecioCorrector(double precioCorrector) {
+        this.precioCorrector = precioCorrector;
+    }
+
+   }
+
+
